ef7b96c321f0ddaad90a664c2f0f0763e2dc560e
nick@ohbees.com.au <nick@ohbees.com.au>
2019-01-18 11:37:57 +1000

    postgres setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,21 +1,26 @@
 # services/users/project/config.py
 
 
+import os
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TESTING_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
